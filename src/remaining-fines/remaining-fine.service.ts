bbaa4c99731595ac38c8b7e674b22799592e9cc9
phongtranWW <tranphong.programmer@gmail.com>
2025-03-19 08:40:56 +0700

    change remaining fine dto

--- src/remaining-fines/remaining-fine.service.ts
+++ src/remaining-fines/remaining-fine.service.ts
@@ -23,13 +23,13 @@ export class RemainingFineService {
         'mv.judgement_id AS "judgementId"',
         'mv.prisoner_id AS "prisonerId"',
         'mv.prisoner_name AS "prisonerName"',
-        'mv.dob AS "dateOfBirth"',
-        'mv.pob AS "placeOfBirth"',
+        'TO_CHAR(mv.dob, \'YYYY-MM-DD\') AS "dob"',
+        'mv.pob',
         'mv.penalty_name AS "penaltyName"',
         'mv.total_fine AS "totalFine"',
         'mv.paid_amount AS "paidAmount"',
         'mv.remaining_amount AS "remainingAmount"',
-        'mv.status AS "paymentStatus"',
+        'mv.status AS "status"',
       ])
       .from('mv_remaining_fine', 'mv');
 
@@ -50,6 +50,6 @@ export class RemainingFineService {
       .limit(limit)
       .offset(limit * (page - 1));
 
-    return queryBuilder.getRawMany();
+    return queryBuilder.getRawMany<RemainingFineDto>();
   }
 }
